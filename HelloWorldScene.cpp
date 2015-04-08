8629699344d5f6fb651dbcc9696285132c613bbc
awofeiguwozaia <76288894@qq.com>
2015-04-08 14:30:51 +0800

    Update HelloWorldScene.cpp

--- HelloWorldScene.cpp
+++ HelloWorldScene.cpp
@@ -6,22 +6,19 @@ Scene* HelloWorld::createScene()
 {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
-    
-    // 'layer' is an autorelease object
+
     auto layer = HelloWorld::create();
 
-    // add layer as a child to scene
     scene->addChild(layer);
 
     // return the scene
     return scene;
 }
 
-// on "init" you need to initialize your instance
+
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
+
     if ( !Layer::init() )
     {
         return false;
